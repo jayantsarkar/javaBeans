bbeff6f5b3f2d2bb9774802b2d330d8a667d0edd
jayant <jayant.sarkar@outlook.com>
2014-01-06 11:42:04 +0530

    1st commit

---
+++ src/com/jayant/javabeans/sampleJavaBeans/FirstJava.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.jayant.javabeans.sampleJavaBeans;
+
+/**
+ * @author jasarkar
+ *
+ */
+public class FirstJava {
+
+	/**
+	 * 
+	 */
+	public FirstJava() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/jayant/javabeans/sampleJavaBeans/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author jasarkar
+ *
+ */
+package com.jayant.javabeans.sampleJavaBeans;
